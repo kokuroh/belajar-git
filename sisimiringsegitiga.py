83eec6050b1ae5b026e1338e5a353311ea6ee4f1
Kokuroh <72267785+kokuroh@users.noreply.github.com>
2020-10-03 00:11:31 +0800

    menghitung sisi miring
    program python pertama saya, fungsinya untuk menghitung sisi miring segitiga sama kaki

---
+++ sisimiringsegitiga.py
@@ -0,0 +1,5 @@
+# Program menghitung panjang sisi miring segitiga sama kaki #
+alas = input("Masukkan panjang alas : ")
+tinggi = input("Masukkan panjang tinggi : ")
+sisimiring = 1/2 * int(alas) * int(tinggi)
+print("Sisi miringnya adalah : " + str(sisimiring))
